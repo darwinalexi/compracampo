f88ae963c4d73ea8a0e9b4193207e7a3222a2522
darwinalexisguerrerobaos <darwinalexisguerrerobaos@gmail.com>
2023-09-23 15:54:10 -0500

    ok

--- js/iniciosecion.js
+++ js/iniciosecion.js
@@ -1,7 +1,7 @@
  function validar() {
 let user = document.getElementById("user").value
 let password=document.getElementById("password").value
-    if (user="darwin"&& password == '1234') {
+    if (user="darwinalexisguerrerobaos@gmail.com"&& password == '1004266831') {
         window.location.href = 'perfil.html';
     }else if (user.length == 0 || password.length==0) {
         alert("Credenciales incorrectas")
