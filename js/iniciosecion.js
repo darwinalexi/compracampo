102f071da03b82ca6049592996283ce856a1bfb8
darwinalexisguerrerobaos <darwinalexisguerrerobaos@gmail.com>
2023-09-23 15:21:26 -0500

    cambio

---
+++ js/iniciosecion.js
@@ -0,0 +1,15 @@
+ function validar() {
+let user = document.getElementById("user").value
+let password=document.getElementById("password").value
+    if (user="darwin"&& password == '1234') {
+        window.location.href = 'perfil.html';
+    }else if (user.length == 0 || password.length==0) {
+        alert("Credenciales incorrectas")
+        }else if(user !='darwin' || password !='1234' && user.length > 0 || password.length >0){
+           alert("sus credenciales son incorrectas por favor vuelva a ingresar")
+        }
+    }
+
+
+    
+ 
